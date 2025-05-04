c387cf959571ae3dcbefb2eb7ef3ce53f490ea3b
socratest3 <socratest3@nycstudents.net>
2025-05-04 11:25:20 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,9 @@
-
+public class Main
+{
+        public static void main(String[] args)
+    {
+      System.out.printn(APCalendar.numberOfLeapYears(2000, 2025));  
+      System.out.printn(APCalendar.dayOfWeek(1, 5, 2019));  
+      System.out.printn(APCalendar.dayOfWeek(1, 10, 2019));  
+    }
+}
